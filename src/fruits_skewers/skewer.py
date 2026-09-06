34396aff6408a244e45df574d4c38e6bc97d7868
HASHIGAYA, Makoto <6903330+makooot@users.noreply.github.com>
2026-09-06 20:44:58 +0900

    refact: modify refrences to imported symbol

--- src/fruits_skewers/skewer.py
+++ src/fruits_skewers/skewer.py
@@ -1,11 +1,14 @@
 import sys
 
-from . import parser_smart, types
-from .types import SkewerParserResult
+from . import parser_smart
+from .types import (
+    SkewerParserResult,
+    SkewerCommandDetail,
+)
 
 
 def skewer_parser(
-    command_detail: types.SkewerCommandDetail,
+    command_detail: SkewerCommandDetail,
     args: list[str] | None = None,
 ) -> SkewerParserResult:
     if args is None:
